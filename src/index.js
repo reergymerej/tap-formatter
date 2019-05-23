349d9fb345b909b9bc8bb1443a0bb41f729e837e
Jeremy Greer <jex.grizzle@gmail.com>
2019-05-22 20:46:28 -0700

    Basically working

--- src/index.js
+++ src/index.js
@@ -4,29 +4,58 @@ const onEvent = (p, eventName) => p.on(eventName, (data) => {
   console.log(`event: ${eventName}\n`, data)
 })
 
+const onFail = (result) => {
+  const { diag, name } = result
+  if (name === 'test exited without ending') {
+    console.log('🤔 when does this test end?')
+    // console.log(result)
+  } else {
+    console.log(diag.at)
+    console.log(`\texpected: ${diag.expected}`)
+    console.log(`\tactual: ${diag.actual}`)
+  }
+}
+
+let start
+
+const onComplete = (result) => {
+  const { ok, pass, fail } = result
+  const time = `${Date.now() - start} ms`
+  let out
+  if (!fail) {
+    out = `👍 x ${pass}`
+  } else {
+    out = `🤔 ${fail}`
+  }
+
+  console.log(`${out} (${time})`)
+}
+
 export default () => {
   // Return a writable stream p that emits parse events.
   const pStream = new Parser()
 
   const events = [
-    'assert',
-    'bailout',
-    'child',
-    'comment',
-    'complete',
-    'extra',
-    'fail',
-    'line',
-    'pass',
-    'plan',
-    'result',
-    'skip',
-    'todo',
-    'version',
+    // 'assert',
+    // 'bailout',
+    // 'child',
+    // 'comment',
+    // 'complete',
+    // 'extra',
+    // 'fail',
+    // 'line',
+    // 'pass',
+    // 'plan',
+    // 'result',
+    // 'skip',
+    // 'todo',
+    // 'version',
   ]
 
   events.forEach(e => onEvent(pStream, e))
+  pStream.on('fail', onFail)
+  pStream.on('complete', onComplete)
 
-  console.log('enjoy your p stream')
+  start = Date.now()
   return pStream
 }
